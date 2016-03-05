e8c2ff55fcfe415c0e14c87f024a00a5be6e0137
Andrew Nadraliev <andrew.nadraliev@hotmail.com>
2016-03-05 17:31:35 +0300

    created form for isProgression

--- FindProgression/Form1.Designer.cs
+++ FindProgression/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.open_file_button = new System.Windows.Forms.Button();
+            this.result_label = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // open_file_button
+            // 
+            this.open_file_button.Location = new System.Drawing.Point(171, 48);
+            this.open_file_button.Name = "open_file_button";
+            this.open_file_button.Size = new System.Drawing.Size(115, 27);
+            this.open_file_button.TabIndex = 0;
+            this.open_file_button.Text = "Открыть файл";
+            this.open_file_button.UseVisualStyleBackColor = true;
+            this.open_file_button.Click += new System.EventHandler(this.open_file_button_Click);
+            // 
+            // result_label
+            // 
+            this.result_label.Location = new System.Drawing.Point(12, 93);
+            this.result_label.Name = "result_label";
+            this.result_label.Size = new System.Drawing.Size(452, 23);
+            this.result_label.TabIndex = 1;
+            this.result_label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(476, 463);
+            this.Controls.Add(this.result_label);
+            this.Controls.Add(this.open_file_button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button open_file_button;
+        private System.Windows.Forms.Label result_label;
     }
 }
 
--- FindProgression/Form1.cs
+++ FindProgression/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Lists;
+using System.IO;
 
 namespace FindProgression
 {
@@ -16,6 +17,35 @@ namespace FindProgression
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void open_file_button_Click(object sender, EventArgs e)
+        {
+            string path = String.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+                string[] lines = File.ReadAllLines(path);
+                MyQueue<double> myQueue = new MyQueue<double>();
+                string[] oneLineSplitted;
+                foreach (string oneLine in lines)
+                {
+                    oneLineSplitted = oneLine.Split(' ');
+                    foreach (string number in oneLineSplitted)
+                        try
+                        {
+                            myQueue.Enqueue(Double.Parse(number));
+                        }
+                        catch (Exception) { }
+                }
+                result_label.Text = "Эта последовательность ";
+                if (myQueue.isArithmeticProgression && myQueue.isGeometricProgression) result_label.Text += "является арифметической и геометрической ";
+                else if (myQueue.isArithmeticProgression) result_label.Text += "является арифметической ";
+                else if (myQueue.isGeometricProgression) result_label.Text += "является геометрической ";
+                else result_label.Text += "не является ";
+                result_label.Text += "прогрессией";
+            }
         }
     }
 }
